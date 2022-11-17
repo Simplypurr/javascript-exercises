f5a4a1ba24a6c9b8739f1c47aec5309d9fa7cfd5
nchkss <nichickevich@gmail.com>
2022-11-17 16:59:36 +0200

    task#5 - done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function (sumFrom, sumTo) {
+ if (sumFrom < 0 || typeof sumTo !== 'number') return 'ERROR';
 
+
+ let sum = 0;
+ let numberFrom = sumFrom;
+ let numberTo = sumTo;
+
+ if (sumFrom > sumTo) {
+  numberFrom = sumTo;
+  numberTo = sumFrom;
+ }
+
+ for (let i = numberFrom; i <= numberTo; i++) {
+  sum += i;
+ }
+ return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
