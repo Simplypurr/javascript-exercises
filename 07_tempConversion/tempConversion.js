3730928bc2751d669eb8c167559ca6be21f9db12
nchkss <nichickevich@gmail.com>
2022-11-17 19:05:43 +0200

    task #7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  const result = Math.round((temp - 32) * (5 / 9) * 10) / 10;
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  const result = +((temp * 1.8) + 32).toFixed(1);
+  return result;
 };
 
 // Do not edit below this line
