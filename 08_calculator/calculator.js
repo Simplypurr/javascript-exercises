1d5c02e2b2d3efc6fa4bedd926a61c545d29d55a
nchkss <nichickevich@gmail.com>
2022-11-29 16:44:18 +0200

    done tasks till 13

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, item) => total += item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, item) => total *= item);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n < 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  };
 };
 
 // Do not edit below this line
